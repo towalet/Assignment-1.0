36e9d266c4db679f0d79c03d22b2edc265bf927f
Anthony Vydysh <avydysh@hotmail.com>
2023-10-03 16:08:28 -0400

    The first game was made for the first role

--- game.py
+++ game.py
@@ -1,2 +1,32 @@
 import role1,role2
 import random
+
+# START OF QUEST
+
+# Mission 1
+
+# Statement shows the characters HP and a quick description of the fist mission
+def startgame1():
+    tries=3
+    print(role1.name,"has spawned in a field full of apple trees, you have to eat 3 apples,\n each apple can randomly take or give you HP the player that has the most HP will win.")
+    while tries>0:
+        print()
+        input("Press [ENTER] to eat an apple: ")
+        apple=random.randint(-2,2)
+        apple2=random.randint(-2,2)
+        print("Before eating the apple your health is",role1.health,"HP")
+        role1.health=role1.health-apple
+        print("After you ate an apple your health has became", role1.health,"HP")
+        role2.health=role2.health-apple2
+        print("And",role2.name,"has",role2.health,"HP")
+        tries=tries-1
+    print()
+    if role1.health>role2.health:
+        print(role1.name,"has won!")
+    elif role1.health<role2.health:
+        print(role2.name,"has won!")
+    else: print("It is a tie")
+    
+
+
+
