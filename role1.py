4acf02fbeee89e8dcbbf7f7eed28cf2ea0925ca7
Anthony Vydysh <avydysh@hotmail.com>
2023-10-08 15:45:37 -0400

    slight update

--- role1.py
+++ role1.py
@@ -1,3 +1,10 @@
+'''
+Anthony Vydysh
+PROG10004 Programming Principles
+Due Date: 10/10/2023
+
+This module Implements the data and logic associated with the first role '''
+
 name="Wizard"
 age=30
 health=10
--- role2.py
+++ role2.py
@@ -1,4 +1,11 @@
-name="Role2" #Witch
+'''
+Anthony Vydysh
+PROG10004 Programming Principles
+Due Date: 10/10/2023
+
+This module Implements the data and logic associated with the second role '''
+
+name="Witch"
 age=40
 health=10
 strength=+1
